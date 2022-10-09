bd778be0aa7663d7a776953bc5fa94d47ca668e7
Bastien VERSINI <bastien.versini@mailbox.org>
2022-10-10 00:17:49 +0200

    fix: backslash

--- keymap_optimot.h
+++ keymap_optimot.h
@@ -1,3 +1,19 @@
+/* Copyright 2022 Bastien VERSINI
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 
 #include "keymap.h"
@@ -85,7 +101,7 @@
 #define OM_NBH  ALGR(OM_MINS) // ‑ NON BREAKING HYPHEN
 #define OM_PLMN ALGR(OM_PLUS) // ±
 #define OM_MUL  ALGR(OM_ASTR) // ×
-#define OM_BSLS ALGR(OM_SLASH)// (backslash)
+#define OM_BSLS ALGR(OM_SLSH) // (backslash)
 #define OM_NEQL ALGR(OM_EQL)  // ≠
 #define OM_LBRC ALGR(OM_LPRN) // [
 #define OM_RBRC ALGR(OM_RPRN) // ]
